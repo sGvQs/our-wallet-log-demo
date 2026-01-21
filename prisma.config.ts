4e2850fa3f021e072b2f37f4adfabae16eeb0c43
su-goto <susum.earth@gmail.com>
2026-01-21 13:43:25 +0900

    [main] これはどうかな？

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,6 @@ const isSQLite = process.env.DATABASE_URL?.startsWith('file:');
 export default defineConfig({
   schema: isSQLite ? "prisma/sqlite/schema.prisma" : "prisma/schema.prisma",
   datasource: {
-    url: isSQLite ? process.env.DATABASE_URL : env('DIRECT_URL'),
+    url: isSQLite ? process.env.DATABASE_URL : (process.env.DIRECT_URL ?? env('DIRECT_URL')),
   },
 });
