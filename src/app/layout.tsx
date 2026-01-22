0d29743b33a630aa32f54eb46f84e0f172a1b2dc
su-goto <susum.earth@gmail.com>
2026-01-22 13:33:45 +0900

    [main] 仮で色設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} ${zenMaruGothic.variable} antialiased`}>
+        <body className={`${zenMaruGothic.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               {children}
