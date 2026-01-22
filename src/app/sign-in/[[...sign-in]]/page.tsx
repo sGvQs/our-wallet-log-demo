debbe03c438e6aeecd2b5647793e08120b9caff2
su-goto <susum.earth@gmail.com>
2026-01-22 22:08:57 +0900

    [main] さぶタイトル追加

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -12,16 +12,26 @@ export default function Page() {
     height: "100vh",
     width: "100vw"
   }}>
-    <div style={{
-      color: "#D97757",
-      display: "flex",
-      gap: "16px",
-      fontFamily: "--font-primary",
-    }}
-    >
-      <HyperText>OUR</HyperText>
-      <HyperText>WALLET</HyperText>
-      <HyperText>LOG</HyperText>
+    <div
+      style={{
+        color: "#D97757",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: "--font-primary",
+        fontSize: "12px"
+      }}>
+      <div style={{
+        display: "flex",
+        gap: "16px"
+      }}
+      >
+        <HyperText>OUR</HyperText>
+        <HyperText>WALLET</HyperText>
+        <HyperText>LOG</HyperText>
+      </div>
+      <h2>同棲やシェアハウス用の家計簿アプリ</h2>
+
     </div>
     <RetroGrid angle={80} cellSize={10} />
     <SignIn />
